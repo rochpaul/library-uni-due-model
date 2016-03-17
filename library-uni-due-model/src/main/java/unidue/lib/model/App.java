f5962ec29d728aae5e337cf3ff91e32c81fb0132
adg167c <adg167c@PCFR13.win.uni-due.de>
2016-03-17 10:42:12 +0100

    initial commit

---
+++ library-uni-due-model/src/main/java/unidue/lib/model/App.java
@@ -0,0 +1,13 @@
+package unidue.lib.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
