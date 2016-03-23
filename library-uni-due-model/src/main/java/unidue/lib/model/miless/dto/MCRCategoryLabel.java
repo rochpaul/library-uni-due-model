e0ae31940036da5a56f8e2c0f5f7a0940aaf0f16
adg167c <adg167c@PCFR13.win.uni-due.de>
2016-03-23 13:02:47 +0100

    -> Cayenne Miless init

---
+++ library-uni-due-model/src/main/java/unidue/lib/model/miless/dto/MCRCategoryLabel.java
@@ -0,0 +1,40 @@
+package unidue.lib.model.miless.dto;
+
+import unidue.lib.model.miless.auto._MCRCategoryLabel;
+import unidue.lib.model.dto.Department;
+
+/*
+ * #%L
+ * Semesterapparate
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 Universitaet Duisburg Essen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+/**
+ * MCRCategory is replaced by Department DTO
+ *
+ * @deprecated use {@link Department} instead.  
+ */
+public class MCRCategoryLabel extends _MCRCategoryLabel {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
