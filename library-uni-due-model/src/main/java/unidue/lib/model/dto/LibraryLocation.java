eb9fe48b728ab04a0edb2da0edd1adab3695b19d
adg167c <adg167c@PCFR13.win.uni-due.de>
2016-03-24 10:49:00 +0100

    -> Only Settings/classes for Cayenne Server

---
+++ library-uni-due-model/src/main/java/unidue/lib/model/dto/LibraryLocation.java
@@ -0,0 +1,32 @@
+package unidue.lib.model.dto;
+
+/*
+ * #%L
+ * Semesterapparate
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 Universitaet Duisburg Essen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import unidue.lib.model.auto._LibraryLocation;
+
+import java.util.List;
+
+public class LibraryLocation extends _LibraryLocation {
+
+    private static final long serialVersionUID = 1L;
+}
