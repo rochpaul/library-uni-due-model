8a7a35ac3aeb2b445c9409254b9f37664601b490
adg167c <adg167c@PCFR13.win.uni-due.de>
2016-04-14 18:19:34 +0200

    -> Test Environment ist starting (Failsafe Plugin, H2DB, Jetty)

---
+++ library-uni-due-model/src/test/java/unidue/lib/model/it/TestIT.java
@@ -0,0 +1,21 @@
+package unidue.lib.model.it;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class TestIT
+    
+{
+   
+
+ 
+    @Test
+    public void testApp()
+    {
+        
+        Assert.assertEquals(true, true);
+//        assertTrue( true );
+    }
+}
