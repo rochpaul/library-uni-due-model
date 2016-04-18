0141a64c5df2a6d2d0eef988b9e8a130d4c56361
adg167c <adg167c@PCFR13.win.uni-due.de>
2016-04-18 16:46:16 +0200

    -> Adding Profiles in Maven for diffrent tests

---
+++ library-uni-due-model/src/integration-test/java/unidue/lib/model/integration_test/ITDummyTest.java
@@ -0,0 +1,16 @@
+package unidue.lib.model.integration_test;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class ITDummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
